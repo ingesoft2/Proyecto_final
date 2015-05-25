1fc03eb3f7c87731913618c728ae33f86c081636
Mateo Badillo <mateobadilloal@gmail.com>
2015-05-25 08:05:08 -0500

    Subido el proyecto

---
+++ app/src/main/java/com/example/mateobadillo/ingesoft/Home_Fragment.java
@@ -0,0 +1,24 @@
+package com.example.mateobadillo.ingesoft;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by mateobadillo on 4/22/15.
+ */
+public class Home_Fragment extends android.support.v4.app.Fragment {
+
+    View rootview;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        rootview = inflater.inflate(R.layout.home_layout,container, false);
+        return rootview;
+    }
+}
---
+++ app/src/main/java/com/example/mateobadillo/ingesoft/Log.java
@@ -0,0 +1,73 @@
+package com.example.mateobadillo.ingesoft;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class Log extends ActionBarActivity implements View.OnClickListener {
+
+    private Button Blogin, Bregistro;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log);
+
+        Blogin = (Button)findViewById(R.id.Blogin);
+        Bregistro = (Button)findViewById(R.id.Bregistrarse);
+
+        listener();
+
+    }
+
+    public void listener()
+    {
+        Blogin.setOnClickListener(this);
+        Bregistro.setOnClickListener(this);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_log, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        if(Blogin.isPressed())
+        {
+            startActivity(new Intent(this, MenuNavegacion.class));
+            finish();
+        }
+
+        if(Bregistro.isPressed())
+        {
+            startActivity(new Intent(this, Registrarse.class));
+            finish();
+        }
+
+    }
+}
